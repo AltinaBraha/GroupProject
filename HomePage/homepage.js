4dd6d04b23852250f43a3789d7c830eb54bef183
Altina Braha <149594836+ab66201@users.noreply.github.com>
2024-09-18 17:39:17 +0200

    Added footer and new section for authors

--- HomePage/homepage.js
+++ HomePage/homepage.js
@@ -29,3 +29,17 @@ var swiper = new Swiper(".new-arrival", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+  var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    effect: "fade",
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+  
