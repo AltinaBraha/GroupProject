24f0a6908666706846b48ec0223e55c167a646d9
Altina Braha <149594836+ab66201@users.noreply.github.com>
2024-09-19 13:39:37 +0200

    Project Completed

--- Botime Artistike/botime.js
+++ Botime Artistike/botime.js
@@ -1,4 +1,13 @@
- 
+
+document.addEventListener("DOMContentLoaded", function () {
+const menuToggle = document.querySelector('.menu-toggle');
+const navUl = document.querySelector('nav ul');
+
+menuToggle.addEventListener('click', function () {
+    navUl.classList.toggle('show');
+});
+});
+
 
 
 
